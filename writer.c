589723281ed9781a7c8cb5be25c39f2f01ac402e
Bence R <bencethegreat@gmail.com>
2025-03-14 11:49:14 +0000

    Writer format improvements

--- writer.c
+++ writer.c
@@ -14,28 +14,28 @@
 //Take messages from console
 //argc is no. of arguments and argv is an array of string for the arguments
 int main(int argc, char *argv[]) {  
-        if (argc < 2) { //means message was not provided
-            printf("Format: %s \"your message\" \n", argv[0]); 
-            printf("Provide a message."); 
-            return 1;
-        }
+    if (argc < 2) { //means message was not provided
+        printf("Format: %s \"your message\" \n", argv[0]); 
+        printf("Provide a message."); 
+        return 1;
+    }
 
     // Open device for writing
     int fd = open(DEVICE_PATH, O_WRONLY); // file descriptor that stores what open() returns
-        if (fd == -1) { 
-            perror("Failed to open device"); 
-            return 1;
-        }
+    if (fd == -1) { 
+        perror("Failed to open device"); 
+        return 1;
+    }
 
-/* https://www.quora.com/How-does-the-write-function-work-in-C-Can-you-explain-this-function */
+    /* https://www.quora.com/How-does-the-write-function-work-in-C-Can-you-explain-this-function */
 
-// Write messages to device 
+    // Write messages to device 
     ssize_t bytes_written = write(fd, argv[1], strlen(argv[1])); //writes the message (argv[1]) to device
-        if (bytes_written < 0) {
-            perror("Write failed");
-        } else {
-            printf("Data written to device: %zu bytes\n", bytes_written);
-        }
+    if (bytes_written < 0) {
+        perror("Write failed");
+    } else {
+        printf("Data written to device: %zu bytes\n", bytes_written);
+    }
 
     close(fd); 
     return 0;
